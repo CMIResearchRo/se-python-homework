e8288727b841ac39e8a370358059447af037d331
CipiIT <caramarinnarcisciprian@yahoo.com>
2021-10-10 12:05:45 +0300

    Update 10.10.21

--- session2/ex25.py
+++ session2/ex25.py
@@ -9,3 +9,22 @@
         Veti printa prima data: [1, 3, 4, 5, 5]
         Veti prina a doua oara: {1, 3, 4, 5}
 """
+
+x = ()
+l = []
+s = {}
+l2 = []
+while x != 'exit':
+    x = (input())
+    if x == 'exit':
+        break
+    l.append(int(x))
+    l2.append(str(x))
+
+print(l)
+
+s = set(l)
+
+print(s)
+
+###
--- session2/ex28.py
+++ session2/ex28.py
@@ -6,3 +6,12 @@
         Veti primi: 5
         Veti printa: 15
 """
+
+x = int(input())
+s = 0
+y = x + 1
+for i in range(0,y):
+    s = s + i
+print(s)
+
+###
