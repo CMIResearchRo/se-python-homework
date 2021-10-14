789eff8b9e1a6bb7608922e17e4384c7e7306d8e
CipiIT <caramarinnarcisciprian@yahoo.com>
2021-10-14 15:23:29 +0300

    update 14.10.21

--- session2/ex22.py
+++ session2/ex22.py
@@ -6,3 +6,13 @@
         Veti primi: 'center'
         Veti printa: 'CeNtEr'
 """
+x = input()
+l = []
+for i in range(len(x)):
+    if i % 2 ==0:
+        l.append(x[i].upper())
+    elif i % 2 == 1:
+        l.append(x[i].lower())
+print("".join(l))
+
+###
