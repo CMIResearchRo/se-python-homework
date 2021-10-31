13524bf661d8000e9b800194c18a8eab56c871d1
CipiIT <caramarinnarcisciprian@yahoo.com>
2021-10-31 20:16:20 +0200

    Update ex19.py

--- session3/ex19.py
+++ session3/ex19.py
@@ -16,3 +16,87 @@
             }
 
 """
+
+import json
+import random
+
+def f(x):
+    x = 'test'
+    return x
+def writeToJSONFile(fileName, data):
+    filePathNameWExt = fileName + ".json"
+    jsonString = json.dumps(data)
+    jsonFile = open(filePathNameWExt, "a")
+    jsonFile.write(jsonString)
+    jsonFile.write("\n")
+    jsonFile.close()
+
+def myValue():    
+    letters = "qwertyuiopasdfghjklzxcvbnm0123456789"
+    nr = [3, 4, 5, 6]
+    nr = random.choice(nr)
+    value = ''.join(random.choice(letters) for i in range(nr))
+    return value
+def myKey():
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    key = random.choice(numbers)
+    return key
+
+
+
+fileName = f("")
+
+data = {
+    myKey() : myValue(),
+    myKey() : myValue(),
+    myKey() : myValue(),
+    myKey() : myValue(),
+    }
+    
+
+    
+    
+writeToJSONFile(fileName, data)
+
+
+
+
+
+
+
+
+
+"""
+import json
+import random
+def f(x):
+    x = 'test'
+    return x
+
+def writeToJSONFile(fileName, data):
+    filePathNameWExt = fileName + '.json'
+    with open(filePathNameWExt, 'a') as fp:
+        json.dump(data, fp)
+        
+    
+def myValue():    
+    letters = "qwertyuiopasdfghjklzxcvbnm0123456789"
+    nr = [3, 4, 5, 6]
+    nr = random.choice(nr)
+    value = ''.join(random.choice(letters) for i in range(nr))
+    return value
+
+def myKey():
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    key = random.choice(numbers)
+    return key
+
+fileName = f("")
+data = {
+myKey() : myValue(),
+myKey() : myValue(),
+myKey() : myValue(),
+myKey() : myValue()
+}
+writeToJSONFile(fileName, data)
+"""
